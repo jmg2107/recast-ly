37a3a05d97d165602586a5aa9131ad13eb268b99
jmg2107 <jenjengoo@gmail.com>
2016-06-24 17:35:55 -0700

    completed the VideoPlayer tests

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@
 
 // ReactDOM.render(<App />, document.getElementById("app"));
 ReactDOM.render(<VideoList videos={window.exampleVideoData}/>, document.getElementById("app"));
+ReactDOM.render(<VideoPlayer video={window.exampleVideoData[0]}/>, document.getElementById("app"));
